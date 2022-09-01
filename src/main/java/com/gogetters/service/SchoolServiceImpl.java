ed1b7f1398969b90dc50bb830f9d2be7c823eef7
naranburulov <naranburulov@gmail.com>
2022-09-01 19:10:42 -0400

    findById method is created

--- src/main/java/com/gogetters/service/SchoolServiceImpl.java
+++ src/main/java/com/gogetters/service/SchoolServiceImpl.java
@@ -2,37 +2,58 @@ package com.gogetters.service;
 
 import com.gogetters.database.Database;
 import com.gogetters.entity.School;
-
+import java.util.Scanner;
 import java.util.List;
 
 public class SchoolServiceImpl implements CRUDService<School>{
+
     @Override
     public School findById(int id) {
         return Database.schoolList.stream()
                 .filter(school -> school.id==id)
                 .findFirst().orElseThrow();
 
-
-
     }
 
     @Override
     public List<School> findAll() {
-        return null;
+        return Database.schoolList;
     }
 
     @Override
     public void save(School school) {
 
+        Database.schoolList.add(school);
+
     }
+
 
     @Override
     public void update(School school) {
+        School sc =Database.schoolList
+                .stream()
+                .filter(p->p.equals(school))
+                .findFirst().orElseThrow();
+        System.out.println("Please enter the new name of the school");
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+
+        sc.setName(s);
+        System.out.println("successful, school's name is updated");
 
     }
 
+
+
     @Override
     public void deleteById(Long id) {
+        School sc=Database.schoolList
+                .stream()
+                .filter(p->p.getId()==id)
+                .findFirst().orElseThrow();
+        Database.schoolList.remove(sc);
+
+
 
     }
 }
