dbb682dfdc1c63bcc5a6fb2e86f260abe4212f9d
reyhaneterzioglu <terzioglureyhane@gmail.com>
2022-09-01 16:56:15 +0200

    first commit

--- src/main/java/com/gogetters/SchoolMainRunner.java
+++ src/main/java/com/gogetters/SchoolMainRunner.java
@@ -1,6 +1,5 @@
 package com.gogetters;
 
-import com.gogetters.database.Database;
 import com.gogetters.entity.Course;
 import com.gogetters.entity.Parent;
 import com.gogetters.entity.School;
@@ -16,18 +15,24 @@ public class SchoolMainRunner {
 //    public static CRUDService<School> schoolService;
 //    public static CRUDService<Course> courseService;
 //    public static CRUDService<Parent> parentService;
-//    public static CRUDService<Student> studentService;
+   public static CRUDService<Student> studentService;
 //
-//    static {
+    static {
 //        SchoolMainRunner.schoolService = new SchoolService();
 //        SchoolMainRunner.courseService = new CourseService();
 //        SchoolMainRunner.parentService = new ParentService();
-//        SchoolMainRunner.studentService = new StudentService();
-//    }
+        SchoolMainRunner.studentService = new StudentService();
+    }
 
     public static void main(String[] args) {
 
+      //  System.out.println(studentService.findById(1));
+
+    //    System.out.println(studentService.findAll());
+
+        for (Student student : studentService.findAll()) {
+            System.out.println(student);
+        }
+
     }
-
-
 }
---
+++ src/main/java/com/gogetters/service/StudentService.java
@@ -0,0 +1,41 @@
+package com.gogetters.service;
+
+import com.gogetters.database.Database;
+import com.gogetters.entity.Student;
+
+import java.util.List;
+
+import static com.gogetters.database.Database.studentList;
+
+public class StudentService implements CRUDService<Student> {
+
+    @Override
+    public Student findById(int id) {
+
+        return studentList
+                .stream()
+                .filter(student -> student.getId() == id)
+                .findFirst().orElseThrow(() -> new RuntimeException("no such a student"));
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return studentList;
+    }
+
+    @Override
+    public void save(Student student) {
+
+        studentList.add(student);
+    }
+
+    @Override
+    public void update(Student student) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
